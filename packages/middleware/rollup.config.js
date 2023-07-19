ba0b403345a7f12cfd091a64bfcd29ce85f28024
D.M <578328962@qq.com>
2023-07-19 16:28:23 +0800

    0.1.1

---
+++ packages/middleware/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+import babel from "@rollup/plugin-babel";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        sourcemap: true,
+        name: "finalx-middleware-esm"
+      },
+      {
+        file: "dist/index.cjs.js",
+        format: "cjs",
+        sourcemap: true
+      }
+    ],
+    plugins: [external(), resolve(), commonjs(), typescript(), terser(), babel()],
+    external: ["react", "@tarojs/taro", "taro-hooks"]
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()]
+  }
+];
