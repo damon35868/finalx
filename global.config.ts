248e38b985f27fb7ee17841cbcf300e6680b8c68
D.M <578328962@qq.com>
2024-04-10 10:56:21 +0800

    feat: 更新模版

--- global.config.ts
+++ global.config.ts
@@ -11,8 +11,8 @@ import { globalConfig } from '@finalx/common'
       // header:{}
       wsCheckUser: false,
       // ENV -> 如需调试，可手动覆盖环境，上线前记得改回自动模式
-      baseUrl: 'https://finalx.com',
-      wsUrl: 'wss://finalx.com',
+      baseUrl: API_LINK,
+      wsUrl: WS_LINK
     },
     middleware: {
       userAuth: {
